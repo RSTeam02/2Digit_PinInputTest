67173ebc23f7ed79300e1b5148a6eaed97b99614
unknown <sakai.jun01@gmail.com>
2017-03-20 15:00:38 +0100

    update

--- modelcon/rndPin.js
+++ modelcon/rndPin.js
@@ -8,20 +8,22 @@
  */
 
 class RandomPin {
+
     generate() {
         let element = Math.floor((Math.random() * 8999) + 1000).toString();
-
+        outer:
         for (let i = 0; i < element.length; i++) {
             for (let j = i; j < element.length - 1; j++) {
                 if (element.charAt(i) === element.charAt(j + 1)) {
                     return this.generate();
-                } else {             
+                } else {
                     if (element.length - 2 === i) {
-                        return element;
+                        break outer;
                     }
                 }
             }
         }
+        return element;
     }
 
 }
